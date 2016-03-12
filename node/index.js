249fc95de8c77fc017d73dbf37fd9f588e3391a3
Zoë Petard <zoe.wap@gmail.com>
2016-03-12 12:59:59 +0100

    Socket Rooms

--- node/index.js
+++ node/index.js
@@ -3,7 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var lonelyPeople = [];
-var clientMap = new Map();
+var clientMap = {};
+var numberRooms = 1;
 
 app.get('/', function(req, res){
   res.sendFile('static.html', {root:'public'});
@@ -11,43 +12,64 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-  connectedPlayer = socket.id;
   console.log('a player connected');
-  roomNum = 0;
-  
-  if (lonelyPeople.length > 0) {
-    roomNum = lonelyPeople.pop();
-  }
-  else {
-    numPlayers = clientMap.size;
-    if (numPlayers % 2 == 0) {
-      numPlayers += 1;
-    }
-    roomNum = Math.ceil(numPlayers/2);
-  }
 
-  clientMap.set(connectedPlayer, roomNum);
-  socket.join('Room' + roomNum);
-  console.log('in room ' + roomNum);
+  assignToRoom(socket);
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    messageAuthor = socket.id;
-    roomToEmit = clientMap.get(messageAuthor);
+    roomToEmit = clientMap.get(socket.id)[1];
     socket.broadcast.to('Room' + roomToEmit).emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
     console.log('player disconnected');
-    disconnectedPlayer = socket.id;
-    roomToEmpty = clientMap.get(disconnectedPlayer);
-    lonelyPeople.push(roomToEmpty);
-    clientMap.delete(disconnectedPlayer);
+    var c1 = socket;
+    var c2;
+
+    //console.log(clientMap);
+    console.log(clientMap.size);
+
+    roomToEmpty = clientMap.get(c1.id)[1];
+    delete clientMap.get(c1.id);
+
+    console.log(clientMap.size);
+
+    for (var i = 0; i < clientMap.size; i++){
+      var socketId = clientMap.get(clientMap.keys[i]);
+      console.log('hello');
+      console.log(clientMap.get(socketId));
+      //console.log(clientMap[socketId]);
+      if (clientMap.get(socketId)[1] == roomToEmpty) {
+        console.log('widow');
+        assignToRoom(clientMap.get(socketId)[0]);
+      }
+    }
+  
 
   });
-
 });
 
+function assignToRoom(socket) {
+  lonelyPeople.push(socket);
+
+  console.log('#lonelyPeople: ' + lonelyPeople.length);
+
+  if (lonelyPeople.length >= 2) {
+    var c1 = lonelyPeople.pop();
+    var c2 = lonelyPeople.pop();
+
+    c1.join('Room' + numberRooms);
+    c2.join('Room' + numberRooms);
+
+    console.log('joined room: ' + numberRooms);
+
+    clientMap.set(c1.id, [c1, numberRooms]);
+    clientMap.set(c2.id, [c2, numberRooms]);
+
+    numberRooms++;
+  }
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
