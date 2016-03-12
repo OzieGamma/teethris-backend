52ee89cbf34670476ac92530d60ff8eb75ca6b2c
Zoë Petard <zoe.wap@gmail.com>
2016-03-12 07:53:54 +0100

    Socket channels.

--- node/index.js
+++ node/index.js
@@ -2,20 +2,63 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var clientList = [];
+var clientMap = new Map();
+
 app.get('/', function(req, res){
   res.sendFile('static.html', {root:'public'});
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+  connectedPlayer = socket.id;
+  assignToRoom(connectedPlayer, socket);
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    messageAuthor = socket.id;
+    roomNum = clientMap.get(messageAuthor);
+    io.to('Room' + roomNum).emit('chat message', msg);
   });
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('player disconnected');
+    disconnectedPlayer = socket.id;
+    roomToEmpty = clientMap.get(messageAuthor);
+    clientMap.remove(disconnectedPlayer);
+
+    for (var [playerId, roomNum] of clientMap.entries) {
+      if (roomNum == roomToEmpty) {
+        clientMap.remove(playerId);
+      }
+    }
+
+    for (var k in clientMap){
+    if (clientMap.hasOwnProperty(k)) {
+         clientMap.remove(playerId);
+    }
+    }
+
+    //assignToRoom(playerId);
+
   });
+
 });
+
+function assignToRoom(playerId, socket) {
+  console.log('a player connected');
+
+  numPlayers = clientMap.size;
+  if (numPlayers % 2 == 0) {
+    numPlayers += 1;
+  }
+  roomNum = numPlayers/2;
+  
+  //connectedPlayer = socket.id;
+  clientMap.set(playerId, roomNum);
+  socket.join('Room' + roomNum);
+  console.log('in room ' + roomNum);
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
