06b08d3686a2d73abdf7074ad51aa58a2558082c
Zoë Petard <zoe.wap@gmail.com>
2016-03-12 08:52:16 +0100

    Lonely people taken care of.

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var clientList = [];
+var lonelyPeople = [];
 var clientMap = new Map();
 
 app.get('/', function(req, res){
@@ -12,47 +12,42 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 
   connectedPlayer = socket.id;
-  assignToRoom(connectedPlayer, socket);
+  console.log('a player connected');
+  roomNum = 0;
+  
+  if (lonelyPeople.length > 0) {
+    roomNum = lonelyPeople.pop();
+  }
+  else {
+    numPlayers = clientMap.size;
+    if (numPlayers % 2 == 0) {
+      numPlayers += 1;
+    }
+    roomNum = Math.ceil(numPlayers/2);
+  }
+
+  clientMap.set(connectedPlayer, roomNum);
+  socket.join('Room' + roomNum);
+  console.log('in room ' + roomNum);
 
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     messageAuthor = socket.id;
-    roomNum = clientMap.get(messageAuthor);
-    io.to('Room' + roomNum).emit('chat message', msg);
+    roomToEmit = clientMap.get(messageAuthor);
+    io.to('Room' + roomToEmit).emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
     console.log('player disconnected');
     disconnectedPlayer = socket.id;
-    roomToEmpty = clientMap.get(messageAuthor);
+    roomToEmpty = clientMap.get(disconnectedPlayer);
+    lonelyPeople.push(roomToEmpty);
     clientMap.delete(disconnectedPlayer);
-
-    for (var k in clientMap){
-    if (clientMap.hasOwnProperty(k)) {
-         clientMap.delete(playerId);
-    }
-    }
-
-    //assignToRoom(playerId);
 
   });
 
 });
 
-function assignToRoom(playerId, socket) {
-  console.log('a player connected');
-
-  numPlayers = clientMap.size;
-  if (numPlayers % 2 == 0) {
-    numPlayers += 1;
-  }
-  roomNum = Math.ceil(numPlayers/2);
-  
-  //connectedPlayer = socket.id;
-  clientMap.set(playerId, roomNum);
-  socket.join('Room' + roomNum);
-  console.log('in room ' + roomNum);
-}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
