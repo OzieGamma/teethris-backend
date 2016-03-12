e6008aeccf8f3a0785b13ab2d6601e1033627c90
Boris Gordts <borisgordts@hotmail.com>
2016-03-12 02:20:21 +0100

    Removed games

--- teethris/db/schema.rb
+++ teethris/db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312004049) do
-
-  create_table "games", force: :cascade do |t|
-    t.integer  "player1"
-    t.integer  "player2"
-    t.boolean  "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160312002800) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
